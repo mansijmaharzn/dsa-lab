a3054e4049dea527ab2350ae5423a481f528425a
mansijmaharzn <mansijmaharzn@gmail.com>
2023-06-20 18:01:50 +0545

    solved without static

--- lab-4/2.c
+++ lab-4/2.c
@@ -1,14 +1,12 @@
 // count digits of a number
 #include<stdio.h>
+#include<math.h>
 
 int countNum(int num) {
-    static int count= 0;
-    if (num == 0) {
-        return count;
-    } else {
-        count++;
-        return  countNum(num/10);
-    }    
+    if (num < 10) // base case
+        return 1;
+    else // recursive case
+        return 1 + countNum(num / 10);
 }
 
 
@@ -18,7 +16,7 @@ int main() {
     printf("Enter the number: ");
     scanf("%d", &num);
     result = countNum(num);   
-    printf("Total digit present in %d is: %d", num, result);
+    printf("Total digit present in %d is: %d\n", num, result);
 
     return 0;
 }
